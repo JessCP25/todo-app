30e80874a8f6f40c918fed938e1bd1eceaf7a2c8
JessCP25 <jpayanoc25@gmail.com>
2024-01-21 17:46:39 -0500

    Renderizando y creando tarea, uso de ng-for

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -14,5 +14,15 @@ export class HomeComponent {
     'Crear Proyecto',
     'Crear componente',
     'Crear servicio'
-  ])
+  ]);
+
+  changeHandler(event: Event) {
+    const input = event.target as HTMLInputElement;
+    const newTask = input.value;
+    this.tasks.update((tasks) => [...tasks, newTask]);
+  }
+
+  deleteTask(index: number) {
+    this.tasks.update((tasks) => tasks.filter((task, position) => position !== index))
+  }
 }
